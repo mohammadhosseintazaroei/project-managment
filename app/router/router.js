1be67a684975db4dcb942870e7369f205621c8d7
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-07-16 10:21:52 -0700

    project routers

---
+++ app/router/router.js
@@ -0,0 +1,16 @@
+const { authRoutes } = require('./auth');
+const { projectRoutes } = require('./project');
+const { teamRoutes } = require('./team');
+const { userRoutes } = require('./user');
+
+const router = require('express').Router();
+
+router.use("/project" , projectRoutes)
+router.use("/team" , teamRoutes)
+router.use("/user" , userRoutes)
+router.use("/auth" , authRoutes)
+
+
+module.exports = {
+    AllRoutes: router
+}
