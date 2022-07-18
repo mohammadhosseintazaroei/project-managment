716a62dca16a47ca9df240beaac8f048f417536f
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-07-18 06:07:59 -0700

    done register system

--- app/http/controllers/auth.controller.js
+++ app/http/controllers/auth.controller.js
@@ -1,15 +1,32 @@
+const { validationResult } = require("express-validator");
+const { UserModel } = require("../../models/user");
+const { hashString } = require("../../modules/functions");
+
 class AuthController {
-    register(){
+    async register(req, res, next) {
+  try {
+    const { username, password, email, mobile } = req.body;
+    const hash_password = hashString(password)
+    const user = await UserModel.create({
+        username, email, password: hash_password, mobile
+    }).catch(err => {
+        if (err?.code == 11000) {
+            throw { status: 400, message: "نام کاربری قبلا در سیستم ثبت  شده است" }
+        }
+    })
+    return res.json(user)
+  } catch (error) {
+    next(error)
+  }
+    }
+    login() {
 
     }
-    login(){
-
-    }
-    resetPassword(){
+    resetPassword() {
 
     }
 }
 
 module.exports = {
-    AuthController:new AuthController()
+    AuthController: new AuthController()
 }
--- app/http/validations/auth.js
+++ app/http/validations/auth.js
@@ -1,1 +1,37 @@
-add
+const { body } = require('express-validator');
+const { UserModel } = require('../../models/user');
+function registerValidator() {
+    return [
+        body("username").notEmpty().isLength({ min: 4, max: 25 }).custom(async (value, ctx) => {
+            if (value) {
+                const usernameRegex = /^[a-z]+[a-z0-9\_\.]/gi;
+                if (usernameRegex.test(value)) {
+                    const user = await UserModel.findOne({ username:value });
+                    if (user) throw "نام کاربری تکراری میباشد"
+                    return true
+                }
+                throw "نام کاربری صحیح نمیباشد"
+            }
+            throw "نام کابری نمیتواند خالی باشد"
+        }),
+        body("email").isEmail().withMessage("ایمیل وارد شده صحیح نمیباشد").custom(async email => {
+            const user = await UserModel.findOne({ email });
+            if (user) throw "ایمیل واردشده تکراری میباشد"
+            return true;
+        }),
+        body("mobile").isMobilePhone("fa-IR").withMessage("شماره مبایل وارد شده صحیح نمیباشد").custom(async mobile => {
+            const user = await UserModel.findOne({ mobile });
+            if (user) throw "شماره موبایل وارد شده قبلا استفاده شده است"
+            return true;
+        }),
+        body("password").isLength({ min: 6, max: 16 }).withMessage("رمز عبور حد اقل باید 6 کارکتر و حداکثر از 16 کرکتر تشکیل شده بشاد").custom((value, ctx) => {
+            if (!value) throw "رمز عبور نمیتواند خالی باشد"
+            if (value !== ctx?.req?.body?.confirm_password) throw "رمز عبور با تکرار آن یکسان نمیباشد"
+            return true
+        })
+    ]
+}
+
+module.exports = {
+    registerValidator
+}
