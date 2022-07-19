a88579b46dae85c3741da7d912c10f60efd12405
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-07-19 08:14:27 -0700

    edit profile information system

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const { UserModel } = require("../../models/user");
+
 class UserController {
     getProfile(req, res, next) {
 
@@ -12,8 +14,30 @@ class UserController {
             next(user)
         }
     }
-    editProfile() {
-
+    async editProfile(req, res, next) {
+        try {
+            let data = { ...req.body };
+            console.log(data);
+            const userID = req.user._id;
+            let fields = ["first_name", "last_name", "skills"];
+            let basValues = ["", " " , ".", null, undefined, 0, -1, NaN, [], {}]
+            Object.entries(data).forEach(([key, value]) => {
+                if (!fields.includes(key)) delete data[key];
+                if (basValues.includes(value)) delete data[key];
+            })
+            console.log(data);
+            const result = await UserModel.updateOne({ _id: userID }, { $set: data })
+            if (result.modifiedCount > 0) {
+                return res.status(200).json({
+                    status: 200,
+                    success: true,
+                    message: "به روز رسانی  با موفقیت انحام شد"
+                })
+            }
+            throw { status: 400, message: "به روز رسانی انجام نشد" }
+        } catch (error) {
+            error(next)
+        }
     }
     addSkiils() {
 
