65b31627d77260c3d95e5e158f301c938a266913
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-07-31 05:57:40 -0700

    add check format condition to express file upload

--- app/modules/express--fileupload.js
+++ app/modules/express--fileupload.js
@@ -5,8 +5,10 @@ const uploadFile = async (req, res, next) => {
     try {
         if (req.file || Object.keys(req.files).length == 0) throw { status: 400, message: "تصویر شاخص پروژه را ارسال کنید" };
         let image = req.files.image;
-        const image_path = path.join(createUploadPath() + Date.now() + path.extname(image.name));
-        req.body.image = image_path;
+        let type = path.extname(image.name);
+        if (![".png", ".jpg", ".jpeg", ".webp"].includes(type)) throw { status: 400, message: "فرمت ارسال شده تصویر صحیح نمیباشد " }
+        const image_path = path.join(createUploadPath() + Date.now() + type);
+        req.body.image = image_path.substring(7);
         const uploadPath = path.join(__dirname, "..", "..", image_path)
         image.mv(uploadPath, (err) => {
             if (err) throw { status: 500, message: "بارگذاری تصویر انجام نشد" }
