6bc3be47b1c287c82b96b3320c0257f7f64636e1
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-07-31 05:58:36 -0700

    done project routes

--- app/router/project.js
+++ app/router/project.js
@@ -1,12 +1,20 @@
 const { ProjectController } = require('../http/controllers/project.controller');
 const { createProjectValidator } = require('../http/validations/project');
-const {expressValidatorMapper} = require("../http/middlewares/checkErrrors")
+const { expressValidatorMapper } = require("../http/middlewares/checkErrrors")
 const { checkLogin } = require('../http/middlewares/autoLogin');
 const { uploadFile } = require('../modules/express--fileupload');
-const fileupload = require('express-fileupload')
+const fileupload = require('express-fileupload');
+const { mongoIDValidator } = require('../http/validations/public');
 const router = require('express').Router();
 
-router.post("/create",fileupload(),checkLogin,uploadFile, createProjectValidator(), expressValidatorMapper, ProjectController.createProject)
+router.post("/create", fileupload(), checkLogin, uploadFile, createProjectValidator(), expressValidatorMapper, ProjectController.createProject)
+router.get("/list", checkLogin, ProjectController.getAllProject)
+router.get("/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, ProjectController.getProjectById)
+router.delete("/remove/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, ProjectController.removeProject)
+router.put("/edit/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, ProjectController.updateProject)
+router.patch("/edit-project-image/:id", fileupload(),checkLogin,uploadFile, mongoIDValidator(), expressValidatorMapper, ProjectController.updateProjectImage)
+
+
 
 
 module.exports = {
