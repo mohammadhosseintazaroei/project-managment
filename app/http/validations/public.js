dd745980273be3b844396dd8a7c49d4a666227e8
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-07-31 05:56:09 -0700

    add check mongo id function to public validators

---
+++ app/http/validations/public.js
@@ -0,0 +1,9 @@
+const {param} = require("express-validator")
+function mongoIDValidator (){
+    return [
+        param("id").isMongoId().withMessage("شناسه ارسال شده صحیح نمیباشد")
+    ]
+}
+module.exports ={
+    mongoIDValidator
+}
