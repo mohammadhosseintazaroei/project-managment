5a67a5c3da426bb4084ce162aa3b7754911c0770
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-07-31 05:54:16 -0700

    use link service for files

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,11 +1,12 @@
 const { UserModel } = require("../../models/user");
+const { createLinkForFiles } = require("../../modules/functions");
 
 class UserController {
     getProfile(req, res, next) {
 
         try {
             const user = req.user;
-            user.profile_image = req.protocol + "://" + req.get("host") + "/" + user.profile_image;
+            user.profile_image =createLinkForFiles(user.profile_image,req)
             return res.status(200).json({
                 status: 200,
                 success: true,
