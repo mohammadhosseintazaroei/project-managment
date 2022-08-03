5470913b8b3eeab63427e629a057933df38ab12f
MHtazaroie753 <m.h.tazaroie753@gmail.com>
2022-08-03 08:56:15 -0700

    done team routers

--- app/http/controllers/team.controller.js
+++ app/http/controllers/team.controller.js
@@ -1,18 +1,98 @@
+const { TeamModel } = require("./../../models/team")
 class TeamController {
-    createTeam(){
+    async createTeam(req, res, next) {
+        try {
+            const { name, description, username } = req.body;
+            const owner = req.user._id;
+            const team = await TeamModel.create({
+                name,
+                description,
+                username,
+                owner
+            })
+            if (!team) throw { status: 500, message: "ایجاد تیم با خطا مواجه شد" }
+            return res.status(201).json({
+                status: 201,
+                success: true,
+                message: "ایجاد تیم با موفقیت انجام شد"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getListOfTeam(req, res, next) {
+        try {
+            const teams = await TeamModel.find({});
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                teams
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getTeamById(req, res, next) {
+        try {
+            const teamID = req.params.id;
+            const team = await TeamModel.findById(teamID);
+            if (!team) throw { status: 404, message: "تیمی یافت نشد" }
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                team
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getMyTeams(req, res, next) {
+        try {
+            const userID = req.user._id;
+            const teams = await TeamModel.find({
+                $or: [
+                    { owner: userID },
+                    { users: userID }
+                ]
+            })
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                teams
+
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async removeTeamById(req, res, next) {
+        try {
+            const teamID = req.params.id;
+            const team = await TeamModel.findById(teamID);
+            if (!team) throw { status: 404, message: "تیمی یافت نشد" }
+            const result = await TeamModel.deleteOne({ _id: teamID });
+            if (result.deletedCount == 0) throw { status: 500, message: "حذف تیم انجام  نشد" };
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                message: "حذف تیم با موفقیت انجام شد"
+            })
+
+        } catch (error) {
+            next(error)
+        }
+    }
+    inviteUserToTeam() {
 
     }
-    inviteUserToTeam(){
+    removeTeam() {
 
     }
-    removeTeam(){
-        
+    updateTeam() {
+
     }
-    updateTeam(){
-        
-    } 
-    removeUserFromTeam(){
-        
+    removeUserFromTeam() {
+
     }
 }
 
--- app/router/team.js
+++ app/router/team.js
@@ -1,4 +1,18 @@
+const { TeamController } = require('../http/controllers/team.controller');
+const { checkLogin } = require('../http/middlewares/autoLogin');
+const { expressValidatorMapper } = require('../http/middlewares/checkErrrors');
+const { mongoIDValidator } = require('../http/validations/public');
+const { createTeamValidator } = require('../http/validations/team');
+
 const router = require('express').Router();
+router.post("/create" , checkLogin ,createTeamValidator() , expressValidatorMapper, TeamController.createTeam)
+router.get("/list" , checkLogin , TeamController.getListOfTeam)
+router.get("/me" , checkLogin , TeamController.getMyTeams)
+router.get("/:id" , checkLogin ,mongoIDValidator(),expressValidatorMapper, TeamController.getTeamById)
+router.delete("/remove/:id" , checkLogin ,mongoIDValidator(),expressValidatorMapper, TeamController.removeTeamById)
+
+
+
 
 module.exports = {
     teamRoutes: router
